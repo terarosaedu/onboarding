66534f3d1d720022cf92ec2cac5b370fa99835d2
terarosaedu <terarosaeducation@gmail.com>
2026-09-04 13:18:34 +0900

    Change ACCESS_CODE from '0910' to '0920'

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ── 비밀번호 설정 ──
-const ACCESS_CODE = '0910'; // ← 원하는 코드로 변경
+const ACCESS_CODE = '0920'; // ← 원하는 코드로 변경
 
 function checkPassword() {
   const input = document.getElementById('pwInput').value.trim();
